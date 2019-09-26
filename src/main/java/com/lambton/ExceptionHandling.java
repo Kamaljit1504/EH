a6fb429f37233a47f6cdb3783ecd2ab7c11a7a67
MacStudent <macstudent@126-iMac05.local>
2019-09-26 18:59:37 -0400

    Initial commit

---
+++ src/main/java/com/lambton/ExceptionHandling.java
@@ -0,0 +1,40 @@
+package com.lambton;
+import java.util.Scanner;
+
+public class ExceptionHandling
+{
+    public static void main(String[] ar)
+    {
+        Scanner sc=new Scanner(System.in);
+
+        int a;
+        System.out.println("Enter A");
+        a=sc.nextInt();
+        int b;
+        System.out.println("Enter B");
+        b=sc.nextInt();
+        int[] x={1,2,3};
+        int c;
+        System.out.println("Start");
+        try {
+            x[4]=1000;
+            c = a / b;
+            System.out.println("C: "+c);
+        }
+        catch (ArithmeticException e)
+        {
+
+            System.out.println("can't Divide with Zero");
+        }
+        catch (ArrayIndexOutOfBoundsException ae)
+        {
+            System.out.println("Array Index out of bound");
+        }
+        catch (Exception E)
+        {
+            System.out.println(E.toString());
+            System.out.println(E.getMessage());
+        }
+        System.out.println("End");
+    }
+}
